75fb8cf1ab9b294739c31c60ce08a0b183e63371
park <park@iMac001.local>
2014-07-04 14:46:17 +0900

    setup GHUnit

---
+++ MYRHTTPSessionTests/MYRHTTPSessionTests.h
@@ -0,0 +1,13 @@
+//
+//  MYRHTTPSessionTests.h
+//  MYRHTTPSession
+//
+//  Created by park on 2014/07/04.
+//
+//
+
+#import <GHAsyncTestCase.h>
+
+@interface MYRHTTPSessionTests : GHAsyncTestCase
+
+@end
---
+++ MYRHTTPSessionTests/MYRHTTPSessionTests.m
@@ -0,0 +1,13 @@
+//
+//  MYRHTTPSessionTests.m
+//  MYRHTTPSession
+//
+//  Created by park on 2014/07/04.
+//
+//
+
+#import "MYRHTTPSessionTests.h"
+
+@implementation MYRHTTPSessionTests
+
+@end
